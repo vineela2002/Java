ff4f6672bbcb6a68f2b8ba902dd6c24702667019
vineela2002 <70526901+vineela2002@users.noreply.github.com>
2021-02-10 06:47:52 +0530

    Add files via upload

--- MergeSort.java
+++ MergeSort.java
@@ -1,74 +1,89 @@
-public class MergeSort
-{
-	public static void main(String args[])
-	{
-		//int a[]; // array reference
-                int a[]={67,56,78,92,34};
-		int n=a.length;
+import java.io.*;
+import java.util.Scanner;
 
-		System.out.println("\n Before sorting...");
-		display(a,n);
-		
-		mergepass(a,0,n-1); // calling sorting function
+public class MergeSort{
 
-		System.out.println("\n After sorting...");
-		display(a,n);
-	}
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // Input size
+        System.out.println("Enter size of the array: ");
+        int n = in.nextInt();
 
-	public static void mergepass(int a[], int lb,int ub)// divide the list into 2 sublists
-	{
-		int mid;
-		if(lb!=ub)// if(a.length!=1)
-		{
-			mid = (lb+ub)/2;
-			mergepass(a,lb,mid); // divide further left sublist
-			mergepass(a,mid+1,ub); // divide further right sublist
-			mergeSort(a,lb,mid,ub);
-		}
-	}
+        // Input array
+        int[] arr = new int[n];
+        System.out.println("Enter the array elements (one by one): ");
+        for (int i = 0; i < n; ++i) {
+            arr[i] = in.nextInt();
+        }
 
-	public static void mergeSort(int a[], int lb,int mid,int ub)
-	{
-		int i; // indicates the starting position of left sublist
-		int j; // indicates the starting position of right sublist
-		int k; // index of temp array (third list)
-		int temp[];
-		temp = new int[20];
-	
-		i = lb;
-		j = mid+1;
-		k = lb;
-		while((i<=mid) && (j<=ub))// as long as there are elements in both sublists, we do merge -->repeat the loop
-		{
-			if(a[i] <= a[j])
-			{
-				temp[k] = a[i];
-				k++; i++;
-			}
-			else
-			{
-				temp[k] = a[j];
-				k++; j++;
-			}		
-		} // MERGE
-		
-		while(i<=mid) // case 2, dumping remaining elements in the sublist1
-		{
-			temp[k] = a[i];
-			k++; i++;
-		}	
-		while(j<=ub)// case 3, dumping remaining elements in the sublist 2
-		{
-			temp[k] = a[j];
-			k++; j++;		
-		}
-		for(i=lb;i<=ub;i++) 	// post all the elements from temp to back orginial array 'a'
-			a[i] = temp[i];
-	}
+        // Apply merge sort algo
+        mergeSort(arr, 0, n - 1);
 
-	public static void display(int a[],int n)
-	{
-		for(int i=0;i<n;i++)
-		 System.out.print(a[i]+"   ");
-	}
+        // Print sorted array
+        System.out.println("Sorted the array elements: ");
+        for (int i = 0; i < n; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void mergeSort(int[] arr, int lb, int ub) {
+        if (lb < ub) {
+            // Find the mid point 
+            int mid = (lb + ub) / 2;
+
+            // Divide the array at mid point and apply mergesort
+            mergeSort(arr, lb, mid);
+            mergeSort(arr, mid + 1, ub);
+
+            // Merge the two sorted arrays
+            merge(arr, lb, mid, ub);
+        }
+    }
+
+    public static void merge(int[] arr, int lb, int mid, int ub) {
+        // Copy the elements in big array into 2 small arrays, inorder to sort inplace
+        
+        int n1 = mid - lb + 1;
+        int n2 = ub - mid;
+
+        // Declare two arrays & Copy the elements
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+
+        for (int i = 0; i < n1; ++i) {
+            left[i] = arr[lb + i];
+        }
+
+        for (int i = 0; i < n2; ++i) {
+            right[i] = arr[mid + i + 1];
+        }
+
+        int i = 0, j = 0, k = lb;
+
+        // Merge
+        while (i < n1 && j < n2) {
+            if (left[i] < right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        // array which are left in left or right arrays
+        while (i < n1) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+
+        while (i < n2) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+    }
 }
